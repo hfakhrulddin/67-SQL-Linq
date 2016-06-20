440b5ba0647c6e6a778c9d08afbd662379958516
Hussen Fakhrulddin <hf556482@gmail.com>
2016-06-19 22:50:51 -0700

    init

---
+++ LINQ/Backup/WindowFormLINQ/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace WindowFormLINQ
+{
+    partial class frmSimpleLINQExample
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLoadFromDatabase = new System.Windows.Forms.Button();
+            this.btnLoadFromCustomObject = new System.Windows.Forms.Button();
+            this.lstCountry = new System.Windows.Forms.ListBox();
+            this.txtCustomerCode = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnLoadFromDatabase
+            // 
+            this.btnLoadFromDatabase.Location = new System.Drawing.Point(20, 158);
+            this.btnLoadFromDatabase.Name = "btnLoadFromDatabase";
+            this.btnLoadFromDatabase.Size = new System.Drawing.Size(120, 23);
+            this.btnLoadFromDatabase.TabIndex = 0;
+            this.btnLoadFromDatabase.Text = "Load from Database";
+            this.btnLoadFromDatabase.UseVisualStyleBackColor = true;
+            this.btnLoadFromDatabase.Click += new System.EventHandler(this.btnLoadFromDatabase_Click);
+            // 
+            // btnLoadFromCustomObject
+            // 
+            this.btnLoadFromCustomObject.Location = new System.Drawing.Point(20, 187);
+            this.btnLoadFromCustomObject.Name = "btnLoadFromCustomObject";
+            this.btnLoadFromCustomObject.Size = new System.Drawing.Size(120, 23);
+            this.btnLoadFromCustomObject.TabIndex = 1;
+            this.btnLoadFromCustomObject.Text = "Load From Object";
+            this.btnLoadFromCustomObject.UseVisualStyleBackColor = true;
+            this.btnLoadFromCustomObject.Click += new System.EventHandler(this.btnLoadFromCustomObject_Click);
+            // 
+            // lstCountry
+            // 
+            this.lstCountry.FormattingEnabled = true;
+            this.lstCountry.Location = new System.Drawing.Point(20, 12);
+            this.lstCountry.Name = "lstCountry";
+            this.lstCountry.Size = new System.Drawing.Size(120, 108);
+            this.lstCountry.TabIndex = 2;
+            // 
+            // txtCustomerCode
+            // 
+            this.txtCustomerCode.Location = new System.Drawing.Point(20, 126);
+            this.txtCustomerCode.Name = "txtCustomerCode";
+            this.txtCustomerCode.Size = new System.Drawing.Size(120, 20);
+            this.txtCustomerCode.TabIndex = 3;
+            // 
+            // frmSimpleLINQExample
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(158, 222);
+            this.Controls.Add(this.txtCustomerCode);
+            this.Controls.Add(this.lstCountry);
+            this.Controls.Add(this.btnLoadFromCustomObject);
+            this.Controls.Add(this.btnLoadFromDatabase);
+            this.Name = "frmSimpleLINQExample";
+            this.Text = "Simplest LINQ Example";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLoadFromDatabase;
+        private System.Windows.Forms.Button btnLoadFromCustomObject;
+        private System.Windows.Forms.ListBox lstCountry;
+        private System.Windows.Forms.TextBox txtCustomerCode;
+    }
+}
+
---
+++ LINQ/Backup/WindowFormLINQ/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
+namespace WindowFormLINQ
+{
+    
+    public partial class frmSimpleLINQExample : Form
+    {
+        public frmSimpleLINQExample()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnLoadFromCustomObject_Click(object sender, EventArgs e)
+        {
+           
+
+           clsCustomer[] Customers = new clsCustomer[] { new clsCustomer { CustomerName = "Raju", CustomerCode = "001" }, new clsCustomer { CustomerName = "Shiv", CustomerCode = "002" } };
+
+            var query = from c in Customers
+                        where c.CustomerCode == txtCustomerCode.Text
+                        select new { c.CustomerCode, c.CustomerName };
+
+            var enumerator = query.GetEnumerator();
+            
+            lstCountry.Items.Clear();
+            
+            if (enumerator.MoveNext())
+            {
+                var customer = enumerator.Current;
+                lstCountry.Items.Add(customer.CustomerName);
+            }
+        }
+
+        private void btnLoadFromDatabase_Click(object sender, EventArgs e)
+        {
+            DataContext db = new DataContext(@"Data Source=.\SQLEXPRESS;AttachDbFilename=D:\SimpleLINQExample\WindowFormLINQ\Country.mdf;Integrated Security=True;Connect Timeout=30;User Instance=True");
+            Table<clsCustomer> Customers = db.GetTable<clsCustomer>();
+
+            var query = from c in Customers
+                        where c.CustomerCode == txtCustomerCode.Text
+                        select new { c.CustomerCode, c.CustomerName };
+
+            var enumerator = query.GetEnumerator();
+            lstCountry.Items.Clear();
+            if (enumerator.MoveNext())
+            {
+                var customer = enumerator.Current;
+                lstCountry.Items.Add(customer.CustomerName);
+            }
+        }
+    }
+    [Table(Name="Customer")]
+    public class clsCustomer
+    {
+        private string _strCustomerName;
+        private string _strCustomerCode;
+        [Column(Name="CustomerName")]
+        public string CustomerName
+        {
+            set
+            {
+                _strCustomerName = value;
+            }
+            get
+            {
+                return _strCustomerName;
+            }
+        }
+        [Column(Name = "CustomerCode")]
+        public string CustomerCode
+        {
+            set
+            {
+                _strCustomerCode = value;
+            }
+            get
+            {
+                return _strCustomerCode;
+            }
+        }
+    }
+}
